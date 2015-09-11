eead3826cb5f2326f764ba272df587a30a8926dc
Rob Lipp <lunarwild@gmail.com>
2015-09-11 12:43:50 -0700

    added a test class

---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Rob Lipp on 2015-09-11.
+//  Copyright (c) 2015 Rob Lipp. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
