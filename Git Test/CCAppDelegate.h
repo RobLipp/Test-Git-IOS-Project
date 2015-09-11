d73517232c4df52aed0a4fa8974f90fc2b176a73
Rob Lipp <lunarwild@gmail.com>
2015-09-11 12:26:17 -0700

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Rob Lipp on 2015-09-11.
+//  Copyright (c) 2015 Rob Lipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Rob Lipp on 2015-09-11.
+//  Copyright (c) 2015 Rob Lipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
